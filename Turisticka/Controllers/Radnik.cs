9a852b5cf1db42dcb4e19bb25b467f64d42348d9
BranislavaMladenovic <44551442+BranislavaMladenovic@users.noreply.github.com>
2019-04-13 22:11:41 +0200

    Add files via upload
    Ovo je klasa radnik a uradila sam dodaj i prikazi radnika

---
+++ Turisticka/Controllers/Radnik.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;  //dodato
+//Branislava
+namespace MajaIvana.Models
+{
+    public class Radnik
+    {
+        public static List<Korisnici> PrikaziKorisnike()
+        {
+            using (ProbnaAgencijaEntities1 dbEntitet = new ProbnaAgencijaEntities1())
+            {
+                List<Korisnici> korisnici = (from k in dbEntitet.Korisnicis
+                                             where k.Uloga==3
+                                             select k ).ToList();
+                return korisnici;
+            }
+        }
+
+        public static void DodajKorisnika(Korisnici korisnik)
+        {
+            using (ProbnaAgencijaEntities1 dbEntitet = new ProbnaAgencijaEntities1())
+            {
+                try
+                {
+                        dbEntitet.Korisnicis.Add(korisnik);
+                        dbEntitet.SaveChanges();
+                }
+                catch (Exception) { }
+            }
+        }
+
+        //ovo meni ne treba zatrebaće za Rezervacije
+
+        //private void PopuniSifreKorisnika()
+        //{
+        //    SelectList items = Radnik.PopuniListuKorisnikaID();
+        //    if (items != null)
+        //    {
+        //        ViewBag.korisnici = items;
+        //    }
+        //}
+    }
+}
